a974bf2d2151cb9a439a28ee1eeeed018de9a958
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-10-27 20:51:05 +0500

    настраиваю дев сервер

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     entry: './dev/main.js',
+    mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: './public/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000
+    }
 };

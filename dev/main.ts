53bca4ba1c0216ad8522d1fda6acce7eef91b5ea
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-10-28 11:03:17 +0500

    добавил ts

---
+++ dev/main.ts
@@ -0,0 +1,9 @@
+interface User {
+    name: string;
+    id: number;
+}
+
+const user: User = {
+    name: "Hayes",
+    id: "2",
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader')
 const path = require('path');
 
 module.exports = {
-    entry: './dev/main.js',
+    entry: './dev/main.ts',
     mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'awesome-typescript-loader'
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             filename: "index.html",
             template: './public/index.html'
-        })
+        }),
+        new CheckerPlugin()
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),

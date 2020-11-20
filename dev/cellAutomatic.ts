522a153708bc9e619206b3f11c80262b16f56647
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-11-20 20:56:57 +0500

    делаю функцию отметки активных клеток

--- dev/cellAutomatic.ts
+++ dev/cellAutomatic.ts
@@ -51,6 +51,9 @@ class CellAutomatic {
         this.drawApi.drawCellMap(this.parametrs.table, this.parametrs.cellSize);
     }
 
+    setSurroundingСells(): void{
+        console.log("какие окружающие клетки будут считаться");
+    }
 
     initEventsPanel(): void {
         document.querySelector('.panel').addEventListener('click', (e)=>{

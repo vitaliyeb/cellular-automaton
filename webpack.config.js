ca71421a40c43b2eceed11e5d8fe8cd7385ca9dc
vitaliyeb <vlvlv.vovov@bk.ru>
2020-12-07 22:33:42 +0300

    билд

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
     entry: './dev/main.js',
-    // mode: 'development',
+    mode: 'production',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
